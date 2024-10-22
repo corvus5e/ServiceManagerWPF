d2474212072d7074e1184e84a2f23717f6300abd
corvus5e <corvus5e@gmail.com>
2024-10-22 22:02:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This a custom Windows service manager.
+Goals:
+    - start/stop batches of services
+    - practice WPF
---
+++ ServiceManagerWPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ServiceProcess;
+
+namespace ServiceManagerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _groupsComboBox.Items.Add("All");
+            _groupsComboBox.SelectedIndex = 0;
+
+            ServiceController[] services = ServiceController.GetServices();
+            foreach(var s in services)
+            {
+                _servicesDataGrid.Items.Add(new { ServiceName = s.DisplayName, ServiceStatus = s.Status.ToString()});
+            }
+        }
+    }
+}
